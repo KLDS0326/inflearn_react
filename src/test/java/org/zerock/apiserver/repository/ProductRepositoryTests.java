e4cef6e45c6756a63e6ee2cfb94152bb9a32389f
myeongpil <audvlf92@naver.com>
2025-07-23 20:41:11 +0900

    QueryDSL, @ElementCollection 처리

--- src/test/java/org/zerock/apiserver/repository/ProductRepositoryTests.java
+++ src/test/java/org/zerock/apiserver/repository/ProductRepositoryTests.java
@@ -6,9 +6,15 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 import org.zerock.apiserver.domain.Product;
+import org.zerock.apiserver.dto.PageRequestDTO;
 
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -20,13 +26,13 @@ public class ProductRepositoryTests {
 
     @Test
     public void testInsert() {
-        Product product = Product.builder().pname("Test").pdesc("Test desc").price(1000).build();
-        product.addImageString(UUID.randomUUID()+"_" + "IMAGE1.jpg");
-        product.addImageString(UUID.randomUUID()+"_" + "IMAGE2.jpg");
+        for(int i = 0; i < 10; i++) {
+            Product product = Product.builder().pname("Test_" + i).pdesc("Test desc_" + i).price(1000).build();
+            product.addImageString(UUID.randomUUID()+"_" + i + "_IMAGE1.jpg");
+            product.addImageString(UUID.randomUUID()+"_" + i + "_IMAGE2.jpg");
 
-        productRepository.save(product);
-
-
+            productRepository.save(product);
+        }
     }
 
     @Transactional
@@ -72,9 +78,22 @@ public class ProductRepositoryTests {
         product.addImageString(UUID.randomUUID()+"_" + "PIMAGE2.jpg");
 
         productRepository.save(product);
+    }
 
+    @Test
+    public void testList() {
+        Pageable pageable = PageRequest.of(0,10, Sort.by("pno").descending());
+        Page<Object[]> result = productRepository.selectList(pageable);
+
+        result.getContent().forEach(arr -> log.info(Arrays.toString(arr)));
 
     }
 
+    @Test
+    public void testSearch() {
+        PageRequestDTO pageRequestDTO= PageRequestDTO.builder().build();
+        productRepository.searchList(pageRequestDTO);
+
+    }
 
 }
